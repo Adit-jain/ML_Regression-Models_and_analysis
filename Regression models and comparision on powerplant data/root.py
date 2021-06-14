426804b58fb771d6380ab0cee92758ae0c5a1d9e
Adit jain <52854428+Adit-jain@users.noreply.github.com>
2021-06-15 00:07:15 +0530

    Add files via upload

---
+++ Regression models and comparision on powerplant data/root.py
@@ -0,0 +1,12 @@
+from multi_reg import Multinomial_Regressor
+from poly_reg import Polynomial_Regressor
+from SVR import Support_Vector_Machine_Regressor
+from Decision_tree import Decision_Tree_Regressor
+from Random_forest import Random_Forest_Regressor
+
+Multinomial_Regressor()
+Polynomial_Regressor()
+Support_Vector_Machine_Regressor()
+Decision_Tree_Regressor()
+Random_Forest_Regressor()
+
